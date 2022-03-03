ba8785bca74297741870413f57e3c31029d2f83d
Bhavesh Vikas Kadam <bhaukadam508@gmail.com>
2022-03-03 18:17:53 +0530

    Adding unicode program

---
+++ Unicod0to9/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//
+//        String zero = "0";
+//        String unicodeZero = "\u0030";
+//        System.out.println(zero);
+//        System.out.println(unicodeZero);
+//        String x=unicodeZero;
+//        for (int i = 1; i < x ; )
+//
+
+
+//        char zero = '0';
+//        char unicodeZero = '\u0030';
+//        System.out.println( zero);
+//        System.out.println(unicodeZero);
+//        System.out.println("=====================================================================");
+//        String numberOneToNine = "0,1,2,3,4,5,6,7,8,9";
+//        String unicodeOneToNine = "\u0030,\u0031,\u0032,\u0033,\u0034,\u0035,\u0036,\u0037,\u0038,\u0039";
+//        System.out.println(numberOneToNine);
+//        System.out.println(unicodeOneToNine);
+//        System.out.println("======================================================================");
+//        System.out.println(" 0 : \u0030 \n 1 : \u0031 \n 2 : \u0032 \n 3 : \u0033 \n 4 : \u0034 \n 5 : \u0035 \n 6 : \u0036 \n 7 : \u0037 \n 8 : \u0038 \n 9 : \u0039 \n");
+        
+    }
+}
